a6b7ced5cf401a2456dcfa71643593eae34850d2
Harsha <mruharsha@gmail.com>
2024-06-23 13:23:36 +0530

    Contact

--- src/pages/UserDetailsForm.jsx
+++ src/pages/UserDetailsForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import '../styles/UserDetailsForm.css';
 
 function UserDetailsForm() {
@@ -8,6 +8,9 @@ function UserDetailsForm() {
     email: '',
     bikeName: '',
   });
+
+  const [message, setMessage] = useState('');
+  const formRef = useRef(null);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -19,61 +22,85 @@ function UserDetailsForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission logic here
-    console.log('Form submitted:', formData);
+    const form = formRef.current;
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbwWaVcz8F1Bi7ick0kwpud3T2jtGeBaO_D4L7y02xef_NFomzRfzmVYtF1J8dz8F8rZ/exec';
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+      .then(response => {
+        if (response.ok) {
+          setMessage("Submitted successfully, You'll receive a call in a short time");
+          setTimeout(() => {
+            setMessage('');
+          }, 5000);
+          setFormData({
+            userName: '',
+            mobileNumber: '',
+            email: '',
+            bikeName: '',
+          });
+        } else {
+          throw new Error('Network response was not ok.');
+        }
+      })
+      .catch(error => {
+        console.error('Error!', error.message);
+        setMessage('There was an error submitting the form.');
+      });
   };
 
   return (
-    <div className="form-container">
-      <h2>Book Your Bike</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="userName">User Name:</label>
-          <input
-            type="text"
-            id="userName"
-            name="userName"
-            value={formData.userName}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="mobileNumber">Mobile Number:</label>
-          <input
-            type="number"
-            id="mobileNumber"
-            name="mobileNumber"
-            value={formData.mobileNumber}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="bikeName">Bike Name:</label>
-          <input
-            type="text"
-            id="bikeName"
-            name="bikeName"
-            value={formData.bikeName}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit" className="submit-button">Submit</button>
-      </form>
-    </div>
+    <>
+      <div className="form-container">
+        <h2>Book Your Bike</h2>
+        <form ref={formRef} onSubmit={handleSubmit} className="submit-to-google-sheet">
+          <div className="form-group">
+            <label htmlFor="userName">User Name:</label>
+            <input
+              type="text"
+              id="userName"
+              name="userName"
+              value={formData.userName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="mobileNumber">Mobile Number:</label>
+            <input
+              type="number"
+              id="mobileNumber"
+              name="mobileNumber"
+              value={formData.mobileNumber}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="bikeName">Bike Name:</label>
+            <input
+              type="text"
+              id="bikeName"
+              name="bikeName"
+              value={formData.bikeName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit" className="submit-button">Submit</button>
+        </form>
+        {message && <p id="msg">{message}</p>}
+      </div>
+    </>
   );
 }
 
